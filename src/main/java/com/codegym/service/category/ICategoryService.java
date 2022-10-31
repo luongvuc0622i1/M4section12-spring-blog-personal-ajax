fdbbe9df7b57d23c60da43838d5c5a9043d67990
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-10-31 16:36:21 +0700

    Revert "clone restful"
    This reverts commit 0df9afbc5926918fc425cca52ec87b7150958308.

--- src/main/java/com/codegym/service/category/ICategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.service.category;
-
-import com.codegym.model.Category;
-import com.codegym.service.IGeneralService;
-
-import java.util.Optional;
-
-public interface ICategoryService extends IGeneralService<Category> {
-}
